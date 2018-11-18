afe6a49f0391337e399bfc4e78fb13a0f3eb7c2f
Andrea Margheri <margheri.andrea@gmail.com>
2018-11-18 20:54:46 +0000

    bond list + weblayout

--- clients/src/main/java/com/template/webserver/Controller.java
+++ clients/src/main/java/com/template/webserver/Controller.java
@@ -2,15 +2,29 @@ package com.template.webserver;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.template.Bond;
+import com.template.UKTFBondState;
+import net.corda.core.contracts.StateAndRef;
+import net.corda.core.flows.FlowException;
 import net.corda.core.identity.CordaX500Name;
 import net.corda.core.messaging.CordaRPCOps;
 import net.corda.core.node.NodeInfo;
+import net.corda.core.node.services.Vault;
+import net.corda.core.node.services.vault.QueryCriteria;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Nullable;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
 
@@ -58,5 +72,50 @@ public class Controller {
                 .collect(toList()));
     }
 
+
+    /**
+     * Returns all the registered peers
+     */
+    @RequestMapping(value = "/bonds", method = RequestMethod.POST, produces = "application/json")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public JSONObject getBonds() {
+
+        JSONObject json = new JSONObject();
+        JSONArray array = new JSONArray();
+
+        Iterator<StateAndRef<UKTFBondState>> i = getUKTFBonds().iterator();
+        int j = 0;
+        while (i.hasNext()) {
+            JSONObject item = new JSONObject();
+            UKTFBondState bond = i.next().getState().getData();
+            item.put("n", j);
+            item.put("id", bond.getBondID());
+            item.put("value", bond.getBondValue());
+            item.put("creditScore", bond.getCreditScore());
+            item.put("ukef", bond.getUKEFSupported());
+            array.add(item);
+            j++;
+        }
+
+
+        json.put("bonds", array);
+
+        logger.info("result values" +  json.toJSONString());
+
+        return json;
+
+    }
+
+    private List<StateAndRef<UKTFBondState>> getUKTFBonds () {
+
+        QueryCriteria.VaultQueryCriteria criteria = new QueryCriteria.VaultQueryCriteria(Vault.StateStatus.UNCONSUMED);
+        Vault.Page<UKTFBondState> results = proxy.vaultQueryByCriteria(criteria, UKTFBondState.class);
+        List<StateAndRef<UKTFBondState>> bonds = results.getStates();
+
+        logger.info("Number of UNCONSUMED bonds " +  bonds.toArray().length);
+
+        return bonds;
+    }
 
 }
--- clients/src/main/resources/static/app.js
+++ clients/src/main/resources/static/app.js
@@ -14,11 +14,64 @@ $(document).ready(function() {
        $('.thisNode').append(data.me.organisation);
 
         if (me == "Exporter"){
-            $('.action').append("<button type=\"button\" class=\"btn btn-outline-success\"> Submit New Application </button>")
+            $('.action').append("<button type=\"button\" class=\"btn btn-primary\" data-toggle=\"modal\" data-target=\"#newApp\">Submit New Bond Application</button>")
         }
 
         if (me == "Exporter"){
-            $('#bond').append("<li class=\"list-group-item\"> ciao exporter </li>");
+
+            console.log("I'm an exporter. I'm retrieving all bond applications")
+
+            $.ajax({
+                type: "POST",
+                url: apiBase + "bonds"
+            }).then(function(res) {
+
+                console.log("retrieved all bonds")
+
+                var str = ""
+
+                res.bonds.forEach(function(item){
+                    str += "<li class=\"list-group-item\" id=\"bond\"" + item.n + ">\n"
+                    str += "<p><span> Bond Name: " + item.id  + "</span> </p>\n"
+                    str += "<p> \n "
+                    str += "<button class=\"btn btn-primary\" type=\"button\" data-toggle=\"collapse\" data-target=\"#bond" + item.n + "Exporter\" aria-expanded=\"false\" aria-controls=\"bond" + item.n + "Exporter\">\n"
+                    str += "Exporter data"
+                    str += "</button>\n"
+                    str += "<button class=\"btn btn-primary\" type=\"button\" data-toggle=\"collapse\" data-target=\"#bond" + item.n + "Bank\" aria-expanded=\"false\" aria-controls=\"bond" + item.n + "Bank\">\n"
+                    str += "Bank data"
+                    str += "</button>\n"
+                    str += "<button class=\"btn btn-primary\" type=\"button\" data-toggle=\"collapse\" data-target=\"#bond" + item.n + "UKEF\" aria-expanded=\"false\" aria-controls=\"bond" + item.n + "UKEF\">\n"
+                    str += "UKEF data"
+                    str += "</button>\n"
+                    str += "</p> \n "
+                    str += " <div class=\"row\">\n"
+                    str += " <div class=\"col\">\n"
+                    str += "<div class=\"collapse multi-collapse\" id=\"bond" + item.n + "Exporter\">\n"
+                    str += "<div class=\"card card-body\">\n"
+                    str += "<p> Bond value: " + item.value + "  </p>\n"
+                    str += "</div>\n"
+                    str += "</div>\n"
+                    str += "</div>\n"
+                    str += " <div class=\"col\">\n"
+                    str += "<div class=\"collapse multi-collapse\" id=\"bond" + item.n + "Bank\">\n"
+                    str += "<div class=\"card card-body\">"
+                    str += "<p> Credit Score: " + item.creditScore + "  </p>\n"
+                    str += "</div>\n"
+                    str += "</div>\n"
+                    str += "</div>\n"
+                    str += " <div class=\"col\">\n"
+                    str += "<div class=\"collapse multi-collapse\" id=\"bond" + item.n + "UKEF\">\n"
+                    str += "<div class=\"card card-body\">\n"
+                    str += "<p> UKEF Support: " + item.ukef + "  </p>\n"
+                    str += "</div>\n"
+                    str += "</div>\n"
+                    str += "</div>\n"
+                    str += "</div>\n"
+                });
+
+                $('#bond').append(str);
+            });
+
         }
 
         if (me == "UKEF"){
@@ -39,5 +92,3 @@ $(document).ready(function() {
     });
 
 });
-
-
